017590a5936f0f8b6e05ad3b39947551ad868ebd
anoop600 <anoop.jain10@gmail.com>
2019-03-07 11:08:47 +0000

    scripts added

---
+++ dev/destroy.sh
@@ -0,0 +1,6 @@
+terraform destroy --auto-approve
+rm ~/.kube/config
+rm aws_auth.yaml
+rm -r .terraform
+rm terraform*
+rm plan.out
--- dev/script.sh
+++
@@ -1,3 +0,0 @@
-terraform output config_map_aws_auth > aws.yaml
-terraform output kubeconfig > ~/.kube/config
-kubectl apply -f aws.yaml
